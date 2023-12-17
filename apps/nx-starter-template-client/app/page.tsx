31b619ed5b971377d762f79b77b1592c925d13ae
MikeXado <mike.gurin21@gmail.com>
2023-12-17 14:35:23 +0200

    🐛 FIX: build errors

--- apps/nx-starter-template-client/app/page.tsx
+++ apps/nx-starter-template-client/app/page.tsx
@@ -1,30 +1,38 @@
 import { initClient } from '@ts-rest/core';
 import { TestTsRestRoute } from '@nx-starter-template/shared-dto-routers';
 export default async function Index() {
-  /**
-   * Initializes the client and sends a request to get a message.
-   * @param {string} TestTsRestRoute - The route for the REST API.
-   * @param {object} options - The options for initializing the client.
-   * @param {string} options.baseUrl - The base URL for the API.
-   * @param {object} options.baseHeaders - The base headers for the API.
-   * @returns {Promise<any>} - A promise that resolves to the result of the API request.
-   */
-  const client = initClient(TestTsRestRoute, {
-    baseUrl: 'http://localhost:3000/api',
-    baseHeaders: {},
-  });
-
-  const result = await client.getMessage({
-    params: {
-      displayName: 'Test1',
-    },
-  });
+  const result = await getMessage();
 
   return (
     <div>
-      <p>
-        {result.status === 200 ? result.body?.message : 'Something went wrong'}
-      </p>
+      <p>{result.message}</p>
     </div>
   );
+}
+
+/**
+ * Retrieves a message from the server.
+ * @returns {Promise<{ message: string | null }>} A promise that resolves to an object containing the message or null if an error occurred.
+ */
+async function getMessage() {
+  try {
+    const client = initClient(TestTsRestRoute, {
+      baseUrl: 'http://localhost:3000/api',
+      baseHeaders: {},
+    });
+
+    const result = await client.getMessage({
+      params: {
+        displayName: 'John Doe',
+      },
+    });
+
+    return {
+      message: result.status === 200 ? result.body?.message : null,
+    };
+  } catch (error) {
+    return {
+      message: null,
+    };
+  }
 }
